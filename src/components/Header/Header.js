386e8f7c6924658d4f54c26ac71913e71940aab4
AlexandreSaja <alexandresaja.01@gmail.com>
2022-05-31 11:54:34 +0200

    premier commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import {  FaArchive } from "react-icons/fa";
+import {  FiLogOut, FiUser, FiStopCircle } from "react-icons/fi";
+import {  RiAdvertisementLine } from "react-icons/ri";
+import { BiCog } from "react-icons/bi";
+import elon from '../../assets/elon.jpeg';
+import "./Header.css";
+import {
+    Link
+} from 'react-router-dom';
+
+
+
+const Header = () => {
+
+    return (
+            <div id="header">
+                <nav>
+                    <div className="logoText">
+                        <img className="imgUser" src={elon} alt={'image user'} />
+                        <p> User Name</p>
+                    </div>
+                    <ul>
+                        <Link to="/users">
+                            <li className="navLeftItem" active="true" > <FiUser /> Utilisateurs</li>
+                        </Link>
+                        <Link to="/missions">
+                            <li className="navLeftItem" ><FaArchive /> Annonces</li>
+                        </Link>
+                        <Link to="/usersSignal">
+                            <li className="navLeftItem" ><RiAdvertisementLine /> Signalement utilisateurs</li>
+                        </Link>
+                        <Link to="/missionsSignal">
+                            <li className="navLeftItem"><FiStopCircle /> Signalements annonces</li>
+                        </Link>
+                        <Link to="/settings">
+                            <li className="navLeftItem" ><BiCog /> Settings</li>
+                        </Link>
+                    </ul>
+                    <button className="deco" ><FiLogOut /> Deconnexion</button>
+                </nav>
+            </div>
+
+
+    );
+};
+
+export default Header;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,46 @@
+import React, {useEffect, useState} from 'react';
+import './NavBar.css';
+import {SearchBar} from "../SearchBar/SearchBar";
+import {useLocation} from "react-router-dom";
+
+const NavBar = () => {
+
+
+    const [title, setTitle] = useState('')
+    const urlName = useLocation().pathname
+
+    useEffect(()=> {
+        /**
+         * Methode pour passer le title de la page sur la NavBar
+         * @param urlName
+         */
+        switch (urlName) {
+            case '/users':
+                return setTitle('Liste Utilisateurs')
+            case '/missions' :
+                return setTitle("Liste Missions")
+            case '/usersSignal' :
+                return setTitle("Utilisateurs Signalés")
+            case '/missionsSignal' :
+                return setTitle("Missions Signalées")
+            case '/settings' :
+                return setTitle("Paramètres")
+            default:
+                return 'Espace Admin'
+        }
+    })
+
+    return (
+        <div id="navBar">
+            <nav className="navContain">
+                <h2>{title}</h2>
+                <div className ="ContainSearch">
+                    <SearchBar/>
+                </div>
+            </nav>
+        </div>
+        )
+
+}
+
+export default NavBar;
